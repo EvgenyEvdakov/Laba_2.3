5d007687da8a567ef9f0acd292116551d9c419f3
Evgeny <kkrutkov02@gmail.com>
2023-04-24 11:25:48 +0300

    Add individual3.py

---
+++ individual3.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+if __name__ == "__main__":
+
+    word = input("Введите слово с точкой в конце: ")
+    num = int(input("Введите номер буквы, после которой нужно вставить новую букву: "))
+    letter = input("Введите букву, которую нужно вставить: ")
+
+    new_word = word[:num] + letter + word[num:]
+    print("Новое слово:", new_word)
+    input("Нажмите на любую кнопку, чтобы выйти!")
