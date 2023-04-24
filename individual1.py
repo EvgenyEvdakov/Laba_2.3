baee275cc009731c2478c8aafe106dbf607d8734
Evgeny <kkrutkov02@gmail.com>
2023-04-24 10:56:49 +0300

    Add individual1.py

---
+++ individual1.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+if __name__ == '__main__':
+
+    s = input("Введите предложение: ")
+    for i in range(2, len(s), 3):
+        print(s[i])
+    print("Третий, шестой и т.д. символы строки: {0}".format(s))
+    input("Нажмите на любую кнопку, чтобы выйти!")
